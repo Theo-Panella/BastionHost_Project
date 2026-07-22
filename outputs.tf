c892c3f586523529f99bb22f86570206c2799f71
Theo-panella <panellatheo@gmail.com>
2026-07-22 14:28:06 -0300

    Add: Outputs for public and private instance IPs

---
+++ outputs.tf
@@ -0,0 +1,9 @@
+output "instance_ip" {
+    description = "The public instances IP address"
+    value       = {for k, instance in aws_instance.instances : k => instance.public_ip}
+}
+
+output "instance_private_ip" {
+    description = "The private instances IP address"
+    value       = {for i, instance in aws_instance.instances : i => instance.private_ip}
+}
